7b7881181825be048690a19b025c675d5b5cfe6d
Devankshi Deshmukh <68735282+curry-dev@users.noreply.github.com>
2025-06-06 21:51:28 -0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,53 @@ import { RouterOutlet, RouterLink, RouterModule } from '@angular/router';
   imports: [
     RouterOutlet,
     RouterModule,
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+  targetX = 0;
+  targetY = 0;
+  imageX = 0;
+  imageY = 0;
+  speed = 5;
+  hasReached = true;
+
+  constructor() {
+    this.animate();
+  }
+
+  onMouseMove(event: MouseEvent) {
+    this.targetX = event.pageX;
+    this.targetY = event.pageY;
+    this.hasReached = false;
+  }
+
+  animate() {
+    const animateStep = () => {
+      const dx = this.targetX - this.imageX;
+      const dy = this.targetY - this.imageY;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance > this.speed) {
+        const angle = Math.atan2(dy, dx);
+        this.imageX += Math.cos(angle) * this.speed;
+        this.imageY += Math.sin(angle) * this.speed;
+        this.hasReached = false;
+      } else {
+        this.imageX = this.targetX;
+        this.imageY = this.targetY;
+        this.hasReached = true;
+      }
+
+      requestAnimationFrame(animateStep);
+    };
+    animateStep();
+  }
+
 
   toggleDarkMode() {
     document.body.classList.toggle('change-mode');
