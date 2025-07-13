14194665dd7037868b482db9187e12948ec6387a
Devankshi Deshmukh <68735282+curry-dev@users.noreply.github.com>
2025-07-12 21:44:15 -0700

    fixed art images

--- src/app/components/curryninja/curryninja.component.ts
+++ src/app/components/curryninja/curryninja.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-curryninja',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './curryninja.component.html',
   styleUrl: './curryninja.component.css'
 })
 export class CurryninjaComponent {
-
+ images: { path: string }[] = [
+   { path: 'animals.jpg' },
+   { path: 'broken.jpg' },
+   { path: 'Delilah.jpg' },
+   { path: 'levi.jpg' },
+   { path: 'itadori + nobara + megumi.jpg' },
+   { path: 'mikey.jpg' },
+   { path: 'pjo-tto.jpg' },
+   { path: 'picnic-final.jpg' },
+   { path: 'megumi-blooming.jpg' },
+   { path: 'cdrama.jpg' },
+   { path: 'canvasdropart-dtiys.jpg' },
+   { path: 'chihiro-haku.jpg' }
+ ];
 }
